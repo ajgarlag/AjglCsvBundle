4d49fa2169f5594d5613b27f039f1fc21594bff9
Antonio J. García Lagar <aj@garcialagar.es>
2013-11-25 19:58:23 +0100

    Fix docblocks

--- DependencyInjection/AjglCsvExtension.php
+++ DependencyInjection/AjglCsvExtension.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * This file is part of the AJ General Libraries Bundles
+ *
+ * Copyright (C) 2010-2013 Antonio J. García Lagar <aj@garcialagar.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Ajgl\Bundle\CsvBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -8,15 +15,10 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ * @author Antonio J. García Lagar <aj@garcialagar.es>
  */
 class AjglCsvExtension extends Extension
 {
-    /**
-     * {@inheritDoc}
-     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,20 +1,22 @@
 <?php
-
+/**
+ * This file is part of the AJ General Libraries Bundles
+ *
+ * Copyright (C) 2010-2013 Antonio J. García Lagar <aj@garcialagar.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Ajgl\Bundle\CsvBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 /**
- * This is the class that validates and merges configuration from your app/config files
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ * @author Antonio J. García Lagar <aj@garcialagar.es>
  */
 class Configuration implements ConfigurationInterface
 {
-    /**
-     * {@inheritDoc}
-     */
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
